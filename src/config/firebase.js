5dbca0f89ddf5bc6cf8a97b606843f92c8b44685
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-28 08:07:45 +0100

    Initial firebase app and config setup

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuGL2Qt9gH_U9qCcjLQcfvJjC2u3F7Hac",
+  authDomain: "photo-tagging-app-a1184.firebaseapp.com",
+  projectId: "photo-tagging-app-a1184",
+  storageBucket: "photo-tagging-app-a1184.appspot.com",
+  messagingSenderId: "603320104716",
+  appId: "1:603320104716:web:bb149d0cdc910011bbfb29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
